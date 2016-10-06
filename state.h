2879e503832dd7cd7508e6d8c908d708563925d1
exonata <renata.smith@live.com>
2016-10-05 17:19:31 -0700

    Add files via upload

---
+++ state.h
@@ -0,0 +1,22 @@
+#ifndef STATE_H_
+#define STATE_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include <newmain.h>
+#include "./BBBIOlib/BBBio_lib/BBBiolib.h"
+
+struct data_t;
+struct test_param_t;
+
+int16_t stateMachine(test_param_t *pParam_t);
+bool bSensorContact(data_t *pSample_t);
+
+#endif
