635277723b106d5045814f684c4ac4f48553fbcc
Renata Smith <renata.smith@live.com>
2016-11-18 12:19:51 -0800

    working

--- src/ADC_Example.c
+++ src/ADC_Example.c
@@ -33,12 +33,14 @@
  //main program  
  int main()  
  {
+	 printf("in main\n");
 	 
 	 
 	 system("echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots");
 	 for(int b = 0; b < 10; b++) 
 	 {
 		 openValve(verticalValve);
+		 printf("in loop\n");
 		 sleep(250);
 		 float adc0 = readADC(0);
 		 printf("LC1: The voltage in mV is %f\n", adc0);
